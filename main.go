db75562504ea584d6410020458e2f1c90123bef3
mister-hai <81480735+mister-hai@users.noreply.github.com>
2021-04-27 12:26:38 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,349 @@
+// This file contains the code for the crypto routines
+package GoDNSexfiltration
+
+import (
+	"bufio"
+	"bytes"
+	"compress/zlib"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
+
+	// Every project needs to have the "go get {REPOSITORY}"
+	// command run for the dependencies
+	"github.com/kevinburke/nacl"
+	"github.com/kevinburke/nacl/secretbox"
+
+	// this is how you overload names for import
+	// Now this is used instead of the internal logger
+	// when you type "log"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/fatih/color"
+)
+
+// bytes per read operation
+var FILEREADSPEED int = 36
+var MADEFOR string = "Church of the Subhacker"
+var BANNERSTRING string = "====== mega impressive banner ======="
+
+func StartLogger(logfile string) (return_code int) {
+	Logs, derp := os.OpenFile(logfile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	LoggerInstance := log.New()
+	Formatter := new(log.TextFormatter)
+	Formatter.ForceColors = true
+	Formatter.FullTimestamp = true
+	Formatter.TimestampFormat = "02-01-2006 15:04:05"
+	LoggerInstance.SetFormatter(Formatter)
+	if derp != nil {
+		// Cannot open log file. Logging to stderr
+		fmt.Sprintf("[-] ERROR: Failure To Open Logfile!", "warn")
+		return 0
+	} else {
+		log.SetOutput(Logs)
+	}
+	return 1
+}
+
+// returns the errors but adds them to a log while printing a
+// message to the screen for your viewing pleasure
+// actions are :panic,alarm,exit,debug
+func ErrorHandling(derp error, message string, action string) error {
+	switch action {
+	case "panic":
+		log.Panic()
+		log.Error(message)
+		//color.Red(message)
+	case "warn":
+		log.Warn()
+	case "fatal":
+		log.Fatal()
+
+	}
+	log.Error(message)
+	color.Red(message)
+	return derp
+}
+
+// shows entries from the logfile, starting at the bottom
+// limit by line number, loglevel, or time
+func ShowLogs(LinesToPrint int, loglevel string, time string) {
+	log.SetFormatter(&log.JSONFormatter{})
+	//switch loglevel{
+	//	case "error":
+	//		log.ErrorLevel
+
+}
+
+// debugging feedback function
+// prints colored text for easy visual identification of data
+// color_int (1,red)(2,green)(3,blue)(4,yellow)
+func DebugPrint(color_int int8, message string, PrintFormat string) {
+	//if color_int
+	switch color_int {
+	//is 1
+	case 1:
+		color.Red(message, PrintFormat)
+		// and so on
+	case 2:
+		color.Blue(message, PrintFormat)
+	case 3:
+		color.Green(message, PrintFormat)
+	case 4:
+		color.Yellow(message, PrintFormat)
+	}
+}
+
+// function to use zlib to compress a byte array
+func ZCompress(input []byte) (herp bytes.Buffer, derp error) {
+	var b bytes.Buffer
+	// feed the writer a buffer
+	w := zlib.NewWriter(&b)
+	// and the Write method will copy data to that buffer
+	// in this case, the input we provide gets copied into the buffer "b"
+	w.Write(input)
+	// and then we close the connection
+	w.Close()
+	// and copy the buffer to the output
+	//copy(herp, b.Bytes())
+	return herp, derp
+}
+
+// compresses []byte with zlib
+func ZDecompress(DataIn []byte) (DataOut []byte) {
+	byte_reader := bytes.NewReader(DataIn)
+	ZReader, derp := zlib.NewReader(byte_reader)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+	copy(DataOut, DataIn)
+	ZReader.Close()
+	return DataOut
+}
+
+// opens files for reading and writing
+func OpenFile(filename string) (filebytes []byte) {
+	// open the file
+	herp, derp := os.Open(filename)
+	if derp != nil {
+		fmt.Sprintf("[-] Could not open File, exiting program", derp)
+	}
+	// function to wait on closing the file
+	defer func() {
+		if derp = herp.Close(); derp != nil {
+			fmt.Sprintf("[-]IO: file already closed, stopped existituating, or de-binarized", "warn")
+		}
+	}()
+	// make io.reader and the buffer it will read into
+	reader := bufio.NewReader(herp)
+	buffer := make([]byte, FILEREADSPEED)
+	for {
+		// read INTO buffer
+		// return bytes read as filebytes
+		_, derp := reader.Read(buffer)
+		if derp != nil {
+			fmt.Sprintf("[-] Could not read from file", derp)
+			break
+		}
+		fmt.Sprintf("[+] Bytes read: %s", filebytes)
+
+	}
+	return buffer
+}
+
+// This function creates a nonce with the bit size set at 24
+// options are:
+//		"gcm"
+//		"salty"
+func NonceGenerator(NonceType string) (nonce []byte, derp error) {
+	switch NonceType{
+	case "gcm":
+		nonce := make([]byte, aesGCM.NonceSize())
+		
+	case "salty" :
+		bitsize := big.NewInt(24)
+		nonce = make([]byte, 24)
+		// make random 24 bit prime number
+		if _, err := rand.Read(nonce); err != nil {
+			fmt.Sprintf("[-] Failed to generate 64-bit Random Number", derp)
+		}
+	}
+	//copy number into buffer
+	// after converting bigint to byte with internal method
+	//copy(nonce, n.Bytes())
+	return nonce, derp
+}
+
+// makes chunky data for packet stuffing
+// chunk size known, number of packets unknown
+func DataChunkerChunkSize(DataIn []byte, chunkSize int) [][]byte { //, derp error) {
+	var DataInLength = len(DataIn)
+	// make the buffer
+	DataOutBuffer := make([][]byte, DataInLength)
+	// loop over the original data object taking a bite outta crim... uh data
+	for asdf := 1; asdf < DataInLength; asdf += chunkSize {
+		// mark the end bounds
+		end := asdf + chunkSize
+		// necessary check to avoid slicing beyond slice capacity
+		if end > DataInLength {
+			end = DataInLength
+		}
+		DataOutBuffer = append(DataOutBuffer, DataIn[asdf:chunkSize])
+
+	}
+
+	return DataOutBuffer
+}
+
+func GCMEncrypter(key []byte, plaintext []byte, nonce []byte) (EncryptedBytes []byte, derp error) {
+	// The key argument should be the AES key, either 16 or 32 bytes to select AES-128 or AES-256.
+	block, derp := aes.NewCipher(key)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+	//if _, derp := io.ReadFull(rand.Reader, nonce); derp != nil {
+	//	fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	//}
+	aesgcm, derp := cipher.NewGCM(block)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	copy(ciphertext, EncryptedBytes)
+	return EncryptedBytes, derp
+}
+
+//AES-256-GCM
+// 32bytes only
+// defaults to : AES256Key-32Characters1234567890
+func GCMDecrypter(key []byte, nonce []byte, CipherText []byte) (plaintext []byte, derp error) {
+	if key == nil {
+		key = []byte("AES256Key-32Characters1234567890")
+	}
+	//ciphertext, _ := hex.DecodeString("f90fbef747e7212ad7410d0eee2d965de7e890471695cddd2a5bc0ef5da1d04ad8147b62141ad6e4914aee8c512f64fba9037603d41de0d50b718bd665f019cdcd")
+	//nonce, _ := hex.DecodeString("bb8ef84243d2ee95a41c6c57")
+	block, derp := aes.NewCipher(key)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+	aesgcm, derp := cipher.NewGCM(block)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+
+	plaintext, derp = aesgcm.Open(nil, nonce, CipherText, nil)
+	if derp != nil {
+		fmt.Sprintf("generic error, fix me plz lol <3!", derp)
+	}
+
+	return plaintext, derp
+}
+
+//uses NaCl library
+func saltycrypt(keystring string, DataIn []byte) []byte {
+	key, derp := nacl.Load(keystring)
+	if derp != nil {
+		panic(derp)
+	}
+	encrypted := secretbox.EasySeal(DataIn, key)
+	return encrypted
+}
+
+// decrypts with NaCl
+func saltDEcrypt(keystring string, data []byte) []byte {
+	key, derp := nacl.Load(keystring)
+	if derp != nil {
+		panic(derp)
+	}
+	decrypted, derp := secretbox.EasyOpen(data, key)
+	if derp != nil {
+		fmt.Sprintf("[-] FATAL ERROR: Could not decrypt data!", derp)
+	}
+	return decrypted
+}
+
+func Decrypt(TypeSelection int, key []byte) {
+
+}
+
+/*/
+
+
+/*/
+// struct to contain the message as octets hidden an ipv6 record set
+var Hex2IPV6Stego struct {
+	ciphertext []byte
+}
+
+//https://stackoverflow.com/questions/35809252/check-if-flag-was-provided-in-go
+// make the idea exist
+type FlagVals struct {
+	set       bool   // true if flag used on command line
+	FlagValue string // empty if used but no value fed to parameter
+}
+
+// give the idea an action
+func (FlagPassed *FlagVals) Set(FlagValue string) error {
+	FlagPassed.FlagValue = str
+	FlagPassed.set = true
+	return nil
+}
+
+//
+func (FlagPassed *FlagVals) String() string {
+	return FlagPassed.FlagValue
+}
+
+//declare the new type as variable
+var filename FlagVals
+var EncryptionKey FlagVals
+var EncryptionType FlagVals
+
+// if both are set, pop a cow on the screen and yell at em
+
+//var serveraddr FlagVals
+
+// set before flag options in main
+func init() {
+	flag.Var(&filename, "filename", "the local file to exfiltrate.")
+	flag.Var(&EncryptionKey, "key", "Encryption Key to use")
+	flag.Var(&EncryptionType, " enctype", "Encryption Type , Can be: aes256gcm / salty ")
+
+}
+func main() {
+	//var debug = flag.Bool("d", false, "enable debugging.")
+	var help = flag.Bool("help", false, "show help.")
+
+	flag.Parse()
+
+	if *help || len(os.Args) == 1 {
+		flag.PrintDefaults()
+		return
+	}
+	// make a buffer
+	key := make([]byte, 32)
+	// copy key into it
+	copy(key, EncryptionKey.FlagValue)
+	// open the file
+	fileobject := OpenFile(filename.FlagValue)
+	DebugPrint(1, "--filename set to %q\n", filename.FlagValue)
+	// encode the key to hex
+	//ENCRYPTIONKEY := hex.EncodeToString()
+	herp, derp := ZCompress(fileobject)
+	if derp != nil {
+		fmt.Sprintf("[-] Could not compress file", derp)
+	}
+	switch EncryptionType.FlagValue {
+	case "aes":
+		nonce, derp := NonceGenerator("gcm")
+		if derp != nil {
+			fmt.Sprintf("[-] Nonce Generation FAILED!", derp)
+		}
+		EncryptedText, derp := GCMEncrypter(key, ,nonce)
+	}
+}
